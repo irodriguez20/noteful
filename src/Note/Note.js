e26223f456b9b9037b522543d14514ff3bb61113
India Rodriguez <carnes.s.ia17@gmail.com>
2019-10-15 23:28:31 -0600

    initial commit

---
+++ src/Note/Note.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './Note.css'
+
+export default function Note(props) {
+    return (
+        <div className='Note'>
+            <h2 className='Note__title'>
+                <Link to={`/note/${props.id}`}>
+                    {props.name}
+                </Link>
+            </h2>
+            <button className='Note__delete' type='button'>
+                <FontAwesomeIcon icon='trash-alt' />
+                {' '}
+                remove
+            </button>
+        </div>
+    )
+}
